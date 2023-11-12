08c261e31dbf002aa110e40b4e656f5a12949edc
carodriguezc <88513746+carodriguezc87@users.noreply.github.com>
2023-11-11 22:52:27 -0500

    Update and rename InferenciaLSTM_v2.ino to InferenciaLSTM_test.ino

--- Firmware MCU/InferenciaLSTM_test/InferenciaLSTM_test.ino
+++ Firmware MCU/InferenciaLSTM_test/InferenciaLSTM_test.ino
@@ -8,18 +8,6 @@
 #include "tensorflow/lite/version.h"
 #include "data.h"
 
-/*
-//#include <TensorFlowLite_ESP32.h>
-#include <ESP_TF.h>
-#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
-#include "tensorflow/lite/micro/kernels/micro_ops.h"
-//#include "tensorflow/lite/micro/all_ops_resolver.h"
-//#include "tensorflow/lite/micro/micro_error_reporter.h"
-#include "tensorflow/lite/micro/micro_interpreter.h"
-#include "tensorflow/lite/micro/system_setup.h"
-#include "tensorflow/lite/schema/schema_generated.h"
-#include "data.h"
-*/
 namespace 
 {         
   tflite::ErrorReporter* error_reporter = nullptr;
@@ -50,65 +38,42 @@ void setup()
     Serial.println("Error al cargar el modelo TFLite");
   }
 
- // This pulls in all the operation implementations we need.
+ // Llama a todas las operaciones necesarias para el modelo LSTM
  static tflite::ops::micro::AllOpsResolver resolver;  
-  /*static tflite::MicroOpResolver micro_mutable_op_resolver;
-  micro_mutable_op_resolver.AddBuiltin(
-    tflite::BuiltinOperator_FULLY_CONNECTED,
-    tflite::ops::micro::Register_FULLY_CONNECTED(),
-    1, 3);
-  micro_mutable_op_resolver.AddBuiltin(
-    tflite::BuiltinOperator_UNIDIRECTIONAL_SEQUENCE_LSTM(),
-    tflite::ops::micro::Register_UNIDIRECTIONAL_SEQUENCE_LSTM());
-    
-  micro_mutable_op_resolver.AddBuiltin( 
-    tflite::BuiltinOperator_STRIDED_SLICE(),
-    tflite::ops::micro::Register_STRIDED_SLICE());
-  */   
- // Build an interpreter to run the model with.
+ // Construye un interprete para correr el modelo.
   static tflite::MicroInterpreter static_interpreter(
       model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
- 
   interpreter = &static_interpreter;
                                              
-  // Asignar memoria  para los tensores del modelo.
+  // Asigna memoria  para los tensores del modelo.
   TfLiteStatus allocate_status = interpreter->AllocateTensors();
   if (interpreter->AllocateTensors() != kTfLiteOk) 
   {
     Serial.println("Error al asignar espacio");
   }
 
-  // Obtain pointers to the model's input and output tensors..
+  // Obtiene los punteros para los tensores de entrada y salida del modelo
   input = interpreter->input(0);
   output = interpreter->output(0);
 
-  //Shape for the LSTM model
+  //Da forma al tensor de entrada del modelo LSTM
   input->dims->data[0] = 1;
   input->dims->data[1] = 18;
   input->dims->data[2] = 9;
 
-  //Copy test data to input tensors
+  //Copia los datos del arreglo "test2" al tensor de entrada
   memcpy(input->data.f, test2, 162 * sizeof(float)); 
 
-  
+  //Invoca la inferencia con el modelo
   TfLiteStatus invoke_status = interpreter->Invoke();
   if (invoke_status != kTfLiteOk) 
   {
     Serial.println("Invoke failed");
   }
-/*
-  if (interpreter->Invoke() != kTfLiteOk) {
-    // Maneja el error de inferencia
-    Serial.println("No se logró la inferencia");
-  }
-*/
   float out = output->data.f[0];
-  //output = interpreter->output(0);
   Serial.print("Predicción del modelo: ");
   Serial.println(out);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
